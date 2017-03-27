2836f03c257c6851ce6277d16ebd55305ec2265c
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:40:54 -0700

    Passes Stringify almost finished with getElementByName()

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,30 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+ 
+ //return value is an array of childNodes
+ 	var nodeArray = [];
+ 	var wholeBody = document.body;
+ 	//console.log(wholeBody.classList);
+  //document is everything
+  function getClassNames(element){
+  	console.log(element.childNodes)
+  	if(element.classList && element.classList.contains(className)){
+  		nodeArray.push(element);
+  	}
+
+  	element.childNodes.forEach(function(x){
+  		getElementsByClassName(x);
+  	})
+  }
+
+  getClassNames(wholeBody);
+ 
+  return nodeArray;
+  	//check if it has a class lsit, then check if the element we are looking for .includes() childNode
+  	//classList
+  	  //childNodes
 };
+
+console.log(getElementsByClassName('targetClassName'));
+//console.log(document.getElementsByClassName('targetClassName'));
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
+// used for stringifyJSON spec
+// hint: JSON does not allow you to stringify functions or
+// undefined values, so you should skip those key/value pairs.
+
+
+// unstringifiableValues = [
+//   {
+//     'functions': function() {},
+//     'undefined': undefined
+//   }
+// ];
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+    var emptyArray = [];
+    var emptyObject = {};
+    var emptyString = '';
+	if (typeof(obj) === 'string' || typeof(obj) === 'number' || typeof(obj) === 'boolean' || obj === null){
+	  emptyString += obj;
+	  if (typeof(obj) === 'string'){
+	  	emptyString = '"' + emptyString + '"';
+	  }
+	  return emptyString;
+	}
+	else if(Array.isArray(obj)){
+      obj.forEach(function(element){
+      emptyArray.push(stringifyJSON(element));
+    });
+      return '[' + emptyArray.join(',') + ']';
+    }
+    else if (typeof(obj) === 'object'){
+	  for (var property in obj){
+        if (property === 'functions' || property === 'undefined'){
+        	return '{}';
+        }
+        var empty = stringifyJSON(property) + ':' + stringifyJSON(obj[property]);
+        emptyArray.push(empty);
+	  }
+	  return '{' + emptyArray.join(',') + '}';
+	}
 };
