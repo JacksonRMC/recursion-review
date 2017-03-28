5cc321fcf040cf96eaa0b968b3ff7ab4b0c2532f
Hack Reactor Student <student@hackreactor.com>
2017-03-27 20:48:54 -0700

    Begining on parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,27 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+ 
+ for(var i = 0; i < json.length + 1; i++){
+ 	var char = json[i];
+ 	if(char === '{'){
+ 		//something for objects
+ 	} else if(char === '['){
+ 		//something for arrays
+ 	} else if(char === '\"'){
+ 		return str.substr(1, string.length - 1);
+ 	} else if(char === 't' && [char, (char+1), (char + 2), (char + 3)].join("") === 'true'){
+ 		return true;
+ 	}else if(char === 'f' && ([char, (char+1), (char + 2), (char + 3), (char + 4)].join("") === 'false')){
+ 		return false;
+ 	} else if(char === 'n' && ([char, (char+1), (char + 2), (char + 3)].join("") === 'null')){
+ 		return null;
+ 	} else {
+ 		return undefined;
+ 	}
+ 	
+
+
+
+ }
 };
